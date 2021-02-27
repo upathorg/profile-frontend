537697432a2cfca06fb4a8a24d46b0419e319ae3
harlyon <harrisonekpobimi@gmail.com>
2021-02-27 16:32:27 +0200

    feat: coursedetails page and rrefactors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'; we will re-introduce later
 
 import Routes from "./pages/Routes";
 import "./styles/index.scss";
 import store from "./store";
-
-// const client = new ApolloClient({
-//   uri: 'http://localhost:3000/graphql', we will re-introduce later
-//   cache: new InMemoryCache()
-// });
 
 const App = () => (
   <Provider store={store}>
